14e5837d548bce215d2d8740dfb2acebbb9fd694
dibyendumajumdar <mobile@majumdar.org.uk>
2026-09-11 14:02:28 +0100

    comment cleanup

--- include/redukti/rayoptics/seq/Interface.h
+++ include/redukti/rayoptics/seq/Interface.h
@@ -47,21 +47,6 @@ namespace redukti::rayoptics::seq {
  *         max_aperture: the maximum aperture radius on the interface
  *
  */
-/**
- * Basic part of a sequential model.
- *
- * The SequentialModel is a sequence of Interfaces and Gaps. The Interface class
- * is a boundary between two adjacent Gaps and their associated media. It
- * specifies several methods that must be implemented to model the optical
- * behavior of the interface.
- *
- * The Interface class addresses the following use cases:
- *   - support for ray intersection calculation during ray tracing
- *     (interfaces can be tilted and decentered wrt the adjacent gaps)
- *   - support for getting and setting the optical power of the interface
- *   - support for various optical properties, i.e. does it reflect or transmit
- *   - supports a basic idea of size, the max_aperture
- */
 class Interface {
 public:
     InteractMode interact_mode = InteractMode::TRANSMIT;
@@ -118,13 +103,6 @@ public:
      *         Returns:
      *             edge_pt: intersection point of rel_dir with the aperture boundary
      */
-    /**
-     * Get a target for ray aiming to aperture boundaries.
-     *
-     * The main use case is iterating a ray to the internal edge of a surface.
-     * Although rel_dir is given as a 2d vector, in practice only the 4 quadrant
-     * axes are handled, a 1D directional search along a coordinate axis.
-     */
     virtual mathlib::Vector2 edge_pt_target(const mathlib::Vector2 &rel_dir) const {
         return rel_dir.normalize().times(max_aperture);
     }
@@ -137,10 +115,6 @@ public:
      *             y: y coodinate of the test point
      *             fuzz: tolerance on test pt/aperture comparison,
      *                   i.e. pt fuzzy <= surface_od
-     */
-    /**
-     * Returns true if the point (x, y) is inside the clear aperture.
-     * `fuzz` is a nullable Double in the Java.
      */
     virtual bool point_inside(double x, double y, std::optional<double> fuzz) const {
         double f = fuzz.has_value() ? *fuzz : 1e-5;
@@ -160,24 +134,14 @@ public:
      *         Args:
      *             p0:  start point of the ray in the interface's coordinate system
      *             d:  direction cosine of the ray in the interface's coordinate system
-     *             z_dir: +1 if propagation positive direction, -1 if otherwise
      *             eps: numeric tolerance for convergence of any iterative procedure
+     *             z_dir: +1 if propagation positive direction, -1 if otherwise
      *
      *         Returns:
      *             tuple: distance to intersection point *s1*, intersection point *p*
      *
      *         Raises:
      *             :exc:`~rayoptics.raytr.traceerror.TraceMissedSurfaceError`
-     */
-    /**
-     * Intersect an Interface, starting from an arbitrary point.
-     *
-     * @param p0 start point of the ray in the interface's coordinate system
-     * @param d direction cosine of the ray in the interface's coordinate system
-     * @param eps numeric tolerance for convergence of any iterative procedure
-     * @param z_dir +1 if propagation positive direction, -1 if otherwise
-     *
-     * Throws TraceMissedSurfaceException.
      */
     virtual elem::surface::IntersectionResult intersect(const mathlib::Vector3 &p0,
                                                         const mathlib::Vector3 &d,
